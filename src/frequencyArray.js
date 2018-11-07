ce1b5556cb76451ced52cfb6911cb5bbd31ba63a
Joshua <joshua.smith4@uoit.net>
2018-11-07 17:15:58 -0500

    update

--- src/frequencyArray.js
+++ src/frequencyArray.js
@@ -1,6 +1,7 @@
 "use strict"
 
-function readFrequency() {
+exports.arrFreq = function readFrequency() {
     const fs = require("fs")
     let arrFreq = fs.readFileSync("../CharOccurences.txt", "UTF-8").split('\n')
+    return arrFreq
 }
