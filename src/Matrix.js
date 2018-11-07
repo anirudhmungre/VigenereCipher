d45bab709957995e83c681e134a00d903c25f288
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-07 12:25:48 -0500

    construct matrix

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,7 +1,14 @@
 class Matrix{
-    constructor(x, y){
+    constructor(x, y, val=null){
         this.x = x
         this.y = y
+        this.arr = [];
+        for(i = 0; i < x; i++) {
+            this.arr[i] = [];
+            for(j = 0; j < y; j++) {
+                this.arr[i][j] = val;
+            }
+        }
     }
     get size(){
         return [this.x, this.y]
