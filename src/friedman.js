ddbbfc4d6424f9d36a1cc8459e75535939329193
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-06 20:47:38 -0500

    push friedman

--- src/friedman.js
+++ src/friedman.js
@@ -11,17 +11,23 @@ function occurrences(etxt){
     }
     return arr
 }
-function friedman(etxt, occur){
+function friedman(etxt){
     let fried, ko, kr, kp
     let sum = 0
+    let occur = occurrences(etxt)
     for (let i = 0; i<occur.length ; i++){
         sum += occur[i] * (occur[i]-1)
     }
+    console.log(sum)
+    console.log(etxt.length*(etxt.length-1))
     ko = sum/(etxt.length*(etxt.length-1))
     kr = 1/occur.length
     kp = 0.067
+    console.log(kp)
+    console.log(ko)
+    console.log(kr)
     fried = (kp-kr)/(ko-kr)
     return fried
 }
-let etxt = "AERYGOOD"
-console.log(friedman(etxt, occurrences(etxt)))
+let etxt = "VHXTEPCSMYOVCBLKNFADHPG"
+console.log(friedman(etxt))
