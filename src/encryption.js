3d8f98b3c64ad9be8184c9b2fab148350afb5141
Matthew Boivin <31860965+MattyB97@users.noreply.github.com>
2018-11-07 19:27:01 -0500

    Commented Encryption

--- src/encryption.js
+++ src/encryption.js
@@ -1,10 +1,14 @@
 "use strict"
-
+// Function used to encrypt plaintext file using the provided key
 exports.enc = function(ptxt, key){
+    //Create a variable responsible for storing the encrypted text
     let etxt = ""
+    //Parse through the plaintext
     for (let i = 0 ; i < ptxt.length ; i++){
-        // console.log(ptxt.charCodeAt(i))
+        // console.log(ptxt.charCodeAt(i)) <-- for debugging
+        //Encrypted text will be built by converting both key and plaintext characters to ASCII and then adding both together before being converted back to Alphanumeric characters
         etxt += String.fromCharCode((((ptxt.charCodeAt(i)+key.charCodeAt(i%key.length))-130)%26)+65)
     }
+    //Function returns encrypted text
     return etxt
 }
