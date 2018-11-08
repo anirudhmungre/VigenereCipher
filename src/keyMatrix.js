c233842c94e804b5531c7def93c95bbc59ea936d
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-07 20:45:40 -0500

    fully working frequency  ready for front end

--- src/keyMatrix.js
+++ src/keyMatrix.js
@@ -3,10 +3,21 @@ function maketrix(etxt, kLen){
     return matrix
 }
 
-exports.posOccur = function (etxt, kLen){
-    let matrix = maketrix(etxt, kLen)
+function posOccur (etxt, kLen){
+    let occur
+    occur = maketrix(etxt, kLen)
     for (let i = 0 ; i<etxt.length ; i++){
-        matrix[etxt.charCodeAt(i)-65][i%kLen] +=1
+        occur[etxt.charCodeAt(i)-65][i%kLen]++
+        occur[26][i%kLen]++
     }
-    return matrix
+    return occur
+}
+
+exports.frequency = function(etxt, kLen){
+    let freq
+    freq = posOccur(etxt, kLen)
+    for (let i = 0 ; i < 26 ; i++){
+        freq[i] = freq[i].map((x, index) => {return x/freq[26][index]})
+    }
+    return freq
 }
