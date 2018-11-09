56018d1ee5fa3e2077e13eb5fccfecc1aabb1be9
Joshua <joshua.smith4@uoit.net>
2018-11-09 16:08:19 -0500

    Key length discovered

---
+++ src/app/friedNew.js
@@ -0,0 +1,54 @@
+"use strict"
+
+function calcIC(index, coset) {
+    let sum = 0,
+    ic = 0,
+    N = coset[26]
+    for (let i = 0; i < (coset.length - 1); i++) { sum += coset[i] * (coset[i] - 1) }
+    ic = sum / (N * (N - 1))
+    return sum / (N * (N - 1))
+}
+
+function calcAvgIC(eKeyLength, etxt) {
+    let sum = 0,
+    avgIc = 0
+    for (let i = 1; i < (eKeyLength + 1); i++) {
+        sum += calcIC(i, buildCoset(i, etxt, eKeyLength))
+    }
+    avgIc = sum / eKeyLength
+    return sum / eKeyLength
+}
+
+function buildCoset(index, etxt, keyLength) {
+    let arr = (new Array(27)).fill(0)
+    let letter = 0
+    for (let i = index; i < etxt.length; i += keyLength) {
+        letter = etxt.charCodeAt(i) - 65
+        arr[letter] += 1
+        arr[26] += 1
+    }
+    return arr
+}
+
+function compareAvgIC (avgIC, eKey) {
+    let icEnglish = 0.0686
+    if (avgIC > icEnglish) {
+        return eKey
+    }
+    else if (Math.abs(icEnglish - avgIC) < 0.013) {
+        return eKey
+    }
+    return -1
+}
+
+function getEstKeyLen(etxt) {
+    let eKeyLen = []
+    for (let i = 0; i < 10; i++) {
+        if (compareAvgIC(calcAvgIC(i, etxt), i) != -1) {
+            eKeyLen.push(i)
+        }
+    }
+    return eKeyLen
+}
+console.log(getEstKeyLen("VVQGYTVVVKALURWFHQACMMVLEHUCATWFHHIPLXHVUWSCIGINCMUHNHQRMSUIMHWZODXTNAEKVVQGYTVVQPHXINWCABASYYMTKSZRCXWRPRFWYHXYGFIPSBWKQAMZYBXJQQABJEMTCHQSNAEKVVQGYTVVPCAQPBSLURQUCVMVPQUTMMLVHWDHNFIKJCPXMYEIOCDTXBJWKQGAN"))
+//console.log(calcAvgIC(1, "VVQGYTVVVKALURWFHQACMMVLEHUCATWFHHIPLXHVUWSCIGINCMUHNHQRMSUIMHWZODXTNAEKVVQGYTVVQPHXINWCABASYYMTKSZRCXWRPRFWYHXYGFIPSBWKQAMZYBXJQQABJEMTCHQSNAEKVVQGYTVVPCAQPBSLURQUCVMVPQUTMMLVHWDHNFIKJCPXMYEIOCDTXBJWKQGAN"))
