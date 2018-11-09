0ec5bffe023d973a10e7b07cb38f3a4e258e5736
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-11-08 21:06:09 -0500

    R E F A C T O R I N G

--- src/app/friedman.js
+++ src/app/friedman.js
@@ -1,28 +1,23 @@
 "use strict"
 
-function occurrences(etxt){
-    let arr=[]
-    arr.length = 26; 
-    arr.fill(0);
-    let letter
-    for (let i = 0 ; i<etxt.length ; i++){
-        letter = etxt.charCodeAt(i)-65
+function occurrences(etxt) {
+    let arr = (new Array(26)).fill(0)
+    let letter = 0
+    for (let i = 0; i < etxt.length; i++) {
+        letter = etxt.charCodeAt(i) - 65
         arr[letter] += 1
     }
     return arr
 }
-function fried(etxt){
-    let fried, ko, kr, kp
-    let sum = 0
-    let occur = occurrences(etxt)
-    for (let i = 0; i<occur.length ; i++){
-        sum += occur[i] * (occur[i]-1)
-    }
-    ko = sum/(etxt.length*(etxt.length-1))
-    kr = 1/occur.length
-    kp = 0.067
-    fried = (kp-kr)/(ko-kr)
-    return fried
+
+function fried(etxt) {
+    let ko, kr, 
+    sum = 0, 
+    occur = occurrences(etxt) 
+    for (let i = 0; i < occur.length; i++) { sum += occur[i] * (occur[i] - 1) }
+    ko = sum / (etxt.length * (etxt.length - 1))
+    kr = 1 / occur.length
+    return (0.067 - kr) / (ko - kr)
 }
 
 exports.fried = fried
