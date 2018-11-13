cd3ec882381b0eea146bc3eaf58a42854b05d25f
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-11-12 19:40:21 -0500

    remove merge artifact

--- src/app/decryption.js
+++ src/app/decryption.js
@@ -1,10 +1,6 @@
 "use strict"
 
-<<<<<<< HEAD
 function decrypt(ptxt, key) {
-=======
-function decrypt(ptxt, key){
->>>>>>> 8821b30711d3a040430c51bfcfeceeec2a732ac6
     let etxt = ""
     for (let i = 0; i < ptxt.length; i++) {
         etxt += String.fromCharCode((((ptxt.charCodeAt(i) - 65) - (key.charCodeAt(i % key.length) - 65) + 26) % 26) + 65)
