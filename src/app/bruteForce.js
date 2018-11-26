141f5f53d935bbdfb6c1fd36d8e8d49f00dd1502
Joshua <joshua.smith4@uoit.net>
2018-11-25 22:30:42 -0500

    fix the brute trash the rest

--- src/app/bruteForce.js
+++ src/app/bruteForce.js
@@ -1,15 +1,9 @@
 "use strict"
-const fried = require("./friedman")
+const fried = require(__dirname + '/friedman.js')
 const fs = require("fs")
-const monogram = require('./monogram.js')
-const bigram = require('./bigrams.js')
-
-const getWords = () => {
-    let text, words
-    text = fs.readFileSync("./app/dictionaryByFreq5000.txt", "utf-8").toUpperCase()
-    words = text.split("\n")
-    return words
-}
+const monogram = require(__dirname + '/monogram.js')
+const bigram = require(__dirname + '/bigrams.js')
+const decrypt = require(__dirname + '/decryption.js')
 
 const toString26 = (num, LETARR) => {
     let decremented, quotient, remainder
@@ -25,35 +19,6 @@ const toString26 = (num, LETARR) => {
     return result
 }
 
-const shift = (etxt, key) => {
-    let dtxt = ""
-    let encLet, keyLet, shift
-    for (let i = 0; i < etxt.length; i++) {
-        encLet = etxt.charCodeAt(i)
-        keyLet = key.charCodeAt(i % key.length)
-        if ((encLet) - (keyLet - 65) < 65) {
-            shift = 91 - Math.abs((encLet - 65) - (keyLet - 65))
-            dtxt += String.fromCharCode(shift)
-        }
-        else { dtxt += String.fromCharCode(encLet - (keyLet - 65)) }
-    }
-    return dtxt
-}
-
-// const checkDec = (dtxt, DICT) => {
-//     let foundSum = 0
-//     let ratio = 0.0
-//     const txtChar = dtxt.length
-//     for (let i = 0; i < DICT.length; i++) {
-//         if (dtxt.search(DICT[i]) != -1) {
-//             foundSum += DICT[i].length
-//             ratio = foundSum / txtChar
-//         }
-//         if (ratio > 0.95) { return true }
-//     }
-//     return false
-// }
-
 const checkDec = (dtxt) => {
     let mSum = monogram.findMonogramSum(dtxt, 0.23),
         bSum = bigram.findBigramSum(dtxt, .77)
@@ -64,7 +29,6 @@ const bruteForce = (etxt) => {
     let dtxt, testKey, kLen, start, freq
     const LETARR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
     const KEYLENS = fried.getEstKeyLen(etxt)
-    // const DICT = getWords()
     for (let i = 0; i < KEYLENS.length; i++) {
         kLen = KEYLENS[i]
         console.log(kLen)
@@ -74,13 +38,8 @@ const bruteForce = (etxt) => {
         }
         testKey = "A".repeat(kLen)
         for (let j = start + 1; testKey.length == kLen; j++) {
-            console.log(testKey)
-            dtxt = shift(etxt, testKey)
-            // if (checkDec(dtxt, DICT)) {
-            //     console.log("Is this decrypted?\nKey: " + testKey + "\nText: " + dtxt)
-            //     // USER INPUT FOR YES OR NO
-            //     return dtxt
-            // }
+            // console.log(testKey)
+            dtxt = decrypt.decrypt(etxt, testKey)
             freq = checkDec(dtxt)
             if (freq < 0.5 && freq > 0) {
                 console.log("Is this decrypted?\nKey: " + testKey + "\nText: " + dtxt)
@@ -91,7 +50,8 @@ const bruteForce = (etxt) => {
         }
     }
 }
-console.time("runtime")
-bruteForce("HIDNCUKVYRWEQIPYJPPGUVHEBNVCXSJVHMLOFCHCJCPRBTPAIUAJREPJFAPRZZHIZGFZMQXFGXFWLHIMXRCCMFVPRWOYTVCSKCSAGJIELVNXXFSCBSQLEDVCKKGMALZDRQOYPXQGVKIPQFJQYQTDTWAKVMTWZIEUSPDVPWLHIGLLCHCJKWIRWIBWMXKWINJFYIALXJXCFSTVEDZRILKVXRADLSIBOZILRZZHIZGFZSPGEAMLWRIAUOXJXCFSTVEGIVXGLCTEJATTWYVMTRRMITWGFNDRBWIAELVRJXFGIAIUAJREPJFAPNGJIMLYUPXCBLCICTFDOPWCTEQWUPXCERGGFDRHXSHUPXCVUTGCESTVJSEVYYYVTREDZHLAZRGEALVGWCLVCGMVZCKYKTXMQLRGXMXKWMQHIDNCUKVYRWEQIPYVQSMCRAMAWJPHTWEIYPWJXRUGESIPDRCHYDZRIQSUKILLLGIQAELSLVVGPYFUAIUAJREPJFAPRZVBMJDVCRGMDUYJUIJQCVZIMMFTWENLVGMBGNCXFWIPFZAKWSJWRAMAWNPWZWXXRLAEVXMYVIZCJPIMPWUDJQAKIMLYSNLCJJXWRWIDRRZVQELCRCHMXYPZGFXCSRZZCKRGUDSLUVDVROZRIQZVWEBHVTTCVZCXMLYTFMGBWIPKZHXCJNPWPWRSMLYSJXGLYPHLGGXGRMITWMJTDRTWIHERAFCWGFZIELVNWERAJILCMJTSDSSDSILYDYEZKPPGUVLMRZFJXNATIYPWJDVAGEKIPKRIMMFJHSQZVLEQUFCWGVVGMLYZCLCJFLRKAESEQOVAPYKJWIAGLAHDGIILCZFIHYQDPHCZVGJCWCKIPQJAICHPPRBKKJTGVNWIRZVGXFWGAIYKLGIMXDPOGFXPHYAJNGFSZCAMMCSFCOFGXFLYTXPGLQPCGWVIRLZCKSHRCHNATZMLYKWIBSZHMCKNWILKLSHCFCNEUZZIIPSSQMROZILNAEZIWWJGELUCDWCTPWIPLYTVCORHRMLYXREKFKIPQITQYJBPFJWZCXFSKCSPVZSEJATTXFAEZMRKFKIPQDJGFGLISDLYTAYQKDLCSIILCJRQFGLJPCRGZIWCDWDLBWRGSFVVPVGKYPPJTVAERWNWILKYTXF")
-console.timeEnd("runtime")
-// exports.bruteForce = bruteForce
+
+// console.time("runtime")
+// bruteForce("VHBUSMTIGIILCSTOPEGTXUTVCSXQFLQMXVHBPGMJAMYONNDHVHXTWBUEUGAGGNVTYIVEWYOKFIGCNRQTAGRECNZWAZGTAGEGILBUHECNZWAZGILCVXTSTVIEGAGFRHDULVLTPGNCGXKTBUAFCZBPG")
+// console.timeEnd("runtime")
+exports.bruteForce = bruteForce
